3ae5cf8f6070d901afb578225ca23a7b1322db27
triggerdesign <trigger3@hotmail.de>
2015-07-06 15:43:08 +0200

    #m-10 test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
-                <div class="title">Mezzo test 23456789</div>
+                <div class="title">Mezzo test 2345678910</div>
             </div>
         </div>
     </body>
