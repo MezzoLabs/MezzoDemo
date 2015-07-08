50f44c3a2e5261083335fc00c3adfb6c5b819b7f
simon.schneider <trigger3@hotmail.de>
2015-07-08 11:32:46 +0200

    Test 3 #me-8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
-                Test 2
+                Test 3
             </div>
         </div>
     </body>
