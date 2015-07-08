198ad3819903573fc3c496bd4efcb5022ab12fbd
simon.schneider <trigger3@hotmail.de>
2015-07-08 14:03:36 +0200

    #me-8 added mezzo files to git

---
+++ packages/MezzoLabs/Mezzo/src/MezzoServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MezzoLabs\Mezzo;
+
+use Illuminate\Support\ServiceProvider;
+
+class MezzoServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
