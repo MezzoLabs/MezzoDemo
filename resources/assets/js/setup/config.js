56e3f1f8772d2e5bfeba2a9f1a77d9c9245fb79d
Marc Vornetran <marc.vornetran@gmail.com>
2015-10-20 12:34:25 +0200

    Fix mezzo logo and change url cockpit -> mezzo

--- resources/assets/js/setup/config.js
+++ resources/assets/js/setup/config.js
@@ -4,6 +4,6 @@ export default config;
 
 /*@ngInject*/ function config($locationProvider, $stateProvider, $urlRouterProvider){
     $locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise('/cockpit');
+    $urlRouterProvider.otherwise('/mezzo');
     states.forEach(state => $stateProvider.state(state.name, state.route));
 }
