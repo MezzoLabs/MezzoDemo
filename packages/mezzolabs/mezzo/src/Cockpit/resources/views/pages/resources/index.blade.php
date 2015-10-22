3fed0b0df2d7b1931f20fd101082239513fbc3d3
Marc Vornetran <marc.vornetran@gmail.com>
2015-10-22 18:59:10 +0200

    Merge angular and blade resource index view

--- packages/mezzolabs/mezzo/src/Cockpit/resources/views/pages/resources/index.blade.php
+++ packages/mezzolabs/mezzo/src/Cockpit/resources/views/pages/resources/index.blade.php
@@ -1,11 +1,64 @@
-<div>
-    <label>Search</label>
-    <input name="search" class="form-control" type="search"/>
+<div class="wrapper">
+
+    <!-- Top Container -->
+    <div class="well">
+
+        <!-- Search -->
+        <input type="search" class="form-control pull-right" style="display: inline-block; width: 200px" placeholder="Search" ng-model="vm.searchText">
+        <!-- Search -->
+
+        <!-- Add new -->
+        <button type="button" class="btn btn-primary" ng-click="vm.create()">
+            <span class="ion-plus"></span>
+            Add new
+        </button>
+        <!-- Add new -->
+
+        <!-- Edit -->
+        <button type="button" class="btn btn-default" ng-disabled="!vm.canEdit()" ng-click="vm.edit()">
+            <span class="ion-edit"></span>
+            Edit
+        </button>
+        <!-- Edit -->
+
+        <!-- Delete selected -->
+        <button type="button" class="btn btn-default" ng-disabled="!vm.canRemove()" ng-click="vm.remove()">
+            <span class="ion-trash-b"></span>
+            Delete
+            <span class="badge" ng-bind="vm.countSelected()"></span>
+        </button>
+        <!-- Delete selected -->
+
+    </div>
+    <!-- Top Container -->
+
+    <!-- Bottom Container -->
+    <div class="well">
+
+        <h1>{{ str_plural($model->name()) }}</h1>
+
+        <table class="table table-striped table-responsive">
+            <thead>
+            <tr>
+                <th>
+                    <input type="checkbox" ng-model="vm.selectAll" ng-change="vm.updateSelectAll()">
+                </th>
+                @foreach($model->attributes() as $attribute)
+                    <th>{{ $attribute->title() }}</th>
+                @endforeach
+            </tr>
+            </thead>
+            <tbody>
+            <tr ng-repeat="model in vm.getModels() track by $index">
+                <td>
+                    <input type="checkbox" ng-model="model._meta.selected">
+                </td>
+                <td ng-repeat="value in vm.getModelValues(model)" ng-bind="value"></td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+    <!-- Bottom Container -->
+
 </div>
-
-<h1>{{ str_plural($model->name()) }}</h1>
-
-<table class="table" data-model="{{ $model->name() }}">
-    @foreach($model->attributes() as $attribute)
-        <th>{{ $attribute->title() }}</th>@endforeach
-</table>
