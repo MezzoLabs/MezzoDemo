3e8e1f97bda9aacc4371ff78741756c6c32ca41e
Marc Vornetran <marc.vornetran@gmail.com>
2015-10-22 10:06:53 +0200

    Use gridster from bower

--- packages/mezzolabs/mezzo/src/Cockpit/resources/views/layouts/default.blade.php
+++ packages/mezzolabs/mezzo/src/Cockpit/resources/views/layouts/default.blade.php
@@ -8,8 +8,8 @@
     <base href="/">
 
     <!-- CSS -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,400italic' rel='stylesheet'
-          type='text/css'>
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,400italic" rel='stylesheet'
+          type="text/css">
     <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href="{{ cockpit_asset('/components/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css') }}"
@@ -17,9 +17,9 @@
     <link href="{{ cockpit_asset('/components/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css') }}"
           rel="stylesheet">
     <link href="{{ cockpit_asset('/components/sweetalert2/dist/sweetalert2.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.gridster/0.5.6/jquery.gridster.min.css" rel="stylesheet">
+    <link href="{{ cockpit_asset('/components/gridster/dist/jquery.gridster.min.css') }}" rel="stylesheet">
     <link href="{{ cockpit_asset('/components/chosen/chosen.min.css') }}" rel="stylesheet">
-    <link href="{{ cockpit_asset('/css/app.css') }}" rel='stylesheet' type="text/css">
+    <link href="{{ cockpit_asset('/css/app.css') }}" rel="stylesheet">
     <!-- CSS -->
 </head>
 <body class="@yield('body-class', 'sidebar-pinned')">
@@ -50,7 +50,7 @@
 <script src="{{ cockpit_asset('/components/bootstrap-sass/assets/javascripts/bootstrap.js')}} "></script>
 <script src="{{ cockpit_asset('/components/x-editable/dist/bootstrap3-editable/js/bootstrap-editable.js')}} "></script>
 <script src="{{ cockpit_asset('/components/select2/dist/js/select2.js')}} "></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.gridster/0.5.6/jquery.gridster.min.js"></script>
+<script src="{{ cockpit_asset('/components/gridster/dist/jquery.gridster.min.js') }}"></script>
 <script src="{{ cockpit_asset('/components/angular/angular.min.js')}} "></script>
 <script src="{{ cockpit_asset('/components/angular-ui-router/release/angular-ui-router.min.js')}} "></script>
 <script src="{{ cockpit_asset('/components/angular-sortable-view/src/angular-sortable-view.min.js')}} "></script>
