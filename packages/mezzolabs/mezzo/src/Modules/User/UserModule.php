5067311a39cb5e13496619966851812d8ca12eb4
Simon Schneider <trigger3@hotmail.de>
2015-10-24 15:25:15 +0200

    Add User module

---
+++ packages/mezzolabs/mezzo/src/Modules/User/UserModule.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace MezzoLabs\Mezzo\Modules\User;
+
+use App\User;
+use MezzoLabs\Mezzo\Core\Modularisation\ModuleProvider;
+
+class UserModule extends ModuleProvider
+{
+    protected $models = [
+        User::class,
+    ];
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Called when module is ready, model reflections are loaded.
+     *
+     * @return mixed
+     */
+    public function ready()
+    {
+        $UserReflection = $this->modelReflectionSets->get(User::class);
+
+        //dd($tutorialReflection->relationships());
+    }
+}
