2002e6ce126ae4a8c61f21881ef9c1ccd00a45a9
Simon Schneider <trigger3@hotmail.de>
2015-10-24 15:44:46 +0200

    Permission role table

--- database/migrations/2015_10_24_134205_create_roles_table.php
+++ database/migrations/2015_10_24_134205_create_roles_table.php
@@ -11,7 +11,16 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+
+            $table->integer('tutorial_id')->unsigned();
+            $table->foreign('tutorial_id')->references('id')->on('tutorials')->onDelete('cascade');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
     }
 
     /**
