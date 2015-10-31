729ed5b0b7ee5f55760cf9db0a046d589b2642ac
Simon Schneider <trigger3@hotmail.de>
2015-10-31 19:53:05 +0100

    Thinking about the right way to implement Relationship controllers again

--- packages/mezzolabs/mezzo/src/Modules/User/Http/ApiControllers/UserRolesApiController.php
+++ packages/mezzolabs/mezzo/src/Modules/User/Http/ApiControllers/UserRolesApiController.php
@@ -8,8 +8,21 @@ use MezzoLabs\Mezzo\Http\Controllers\ApiController;
 
 class UserRolesApiController extends ApiController
 {
-    public function index()
+
+    public function index($user_id)
     {
-        
+
     }
+
+    public function show($user_id, $role_id)
+    {
+
+    }
+
+    public function attach($user_id, $role_id)
+    {
+
+    }
+
+
 }
