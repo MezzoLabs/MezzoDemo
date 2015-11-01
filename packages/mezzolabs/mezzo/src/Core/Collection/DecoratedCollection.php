77bb91347589cd9d7182f859cfd04c81b1fcfed5
Simon Schneider <trigger3@hotmail.de>
2015-11-01 18:51:39 +0100

    Store input and database values in extra classes.

---
+++ packages/mezzolabs/mezzo/src/Core/Collection/DecoratedCollection.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace MezzoLabs\Mezzo\Core\Collection;
+
+use Illuminate\Support\Collection as IlluminateCollection;
+
+abstract class DecoratedCollection
+{
+    /**
+     * @var IlluminateCollection
+     */
+    protected $collection;
+
+    public function __construct(IlluminateCollection $collection = null)
+    {
+        if(!$collection)
+            $collection = new IlluminateCollection();
+
+        $this->collection = $collection;
+    }
+
+    /**
+     * @return IlluminateCollection
+     */
+    public function collection()
+    {
+        return $this->collection;
+    }
+
+
+    /**
+     * Execute a callback over each item.
+     *
+     * @param  callable  $callback
+     * @return $this
+     */
+    public function each(callable $callback)
+    {
+       return $this->collection()->each($callback);
+    }
+
+    /**
+     * Run a filter over each of the items.
+     *
+     * @param  callable|null  $callback
+     * @return static
+     */
+    public function filter(callable $callback = null)
+    {
+        return new static($this->collection()->filter($callback));
+    }
+
+    /**
+     * Put an item in the collection by key.
+     *
+     * @param  mixed  $key
+     * @param  mixed  $value
+     * @return $this
+     */
+    public function put($key, $value)
+    {
+        $this->collection()->put($key, $value);
+    }
+}
--- packages/mezzolabs/mezzo/src/Core/Schema/Attributes/Values.php
+++ packages/mezzolabs/mezzo/src/Core/Schema/Attributes/Values.php
@@ -4,7 +4,38 @@
 namespace MezzoLabs\Mezzo\Core\Schema\Attributes;
 
 
-class Values
-{
 
+use Illuminate\Support\Facades\Input;
+use MezzoLabs\Mezzo\Core\Collection\DecoratedCollection;
+use MezzoLabs\Mezzo\Core\Collection\StrictCollection;
+use MezzoLabs\Mezzo\Core\Schema\ModelSchema;
+
+class Values extends StrictCollection
+{
+    public function add(Value $value)
+    {
+        $this->put($value->attribute()->name(), $value);
+    }
+
+
+    protected function checkItem($value)
+    {
+        return $value instanceof Value::class;
+    }
+
+    protected function fromModel(){
+        $values = new Values();
+    }
+
+    public static function fromInput(ModelSchema $model, $data = []){
+        $values = new Values();
+
+        if(empty($data))
+            $data = Input::all();
+
+        foreach($data as $key => $value){
+            //TODO: Go through every dat
+                    }
+
+    }
 }
