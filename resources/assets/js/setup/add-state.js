9b2f5b322432e3cfe7397a3c6a5db8134a8cd972
Marc Vornetran <marc.vornetran@gmail.com>
2015-11-03 14:13:40 +0100

    Register Mezzo Module routes at runtime

---
+++ resources/assets/js/setup/add-state.js
@@ -0,0 +1,9 @@
+export default app => {
+    app.provider('addState', /*@ngInject*/ function($stateProvider){
+        this.$get = () => addState;
+
+        function addState(state){
+            $stateProvider.state(state.name, state.route);
+        }
+    });
+};
