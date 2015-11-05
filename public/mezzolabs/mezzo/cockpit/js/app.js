f917490f204c30ac90bafbc7a70514df199e1ca6
Marc Vornetran <marc.vornetran@gmail.com>
2015-11-05 11:01:29 +0100

    work on resource index view

--- public/mezzolabs/mezzo/cockpit/js/app.js
+++ public/mezzolabs/mezzo/cockpit/js/app.js
@@ -143,10 +143,10 @@ function directive($stateProvider) {
 
     function link(scope, element, attributes) {
         var uri = attributes.uri;
-        var title = attributes.title;
+        var page = attributes.page;
         var action = attributes.action;
         var controller = mapActionToController(action);
-        var state = new _State2['default'](title, uri, {
+        var state = new _State2['default'](page, uri, {
             main: {
                 templateUrl: '/mezzo/' + uri + '.html',
                 controllerAs: 'vm',
@@ -1780,6 +1780,8 @@ module.exports = function (app) {
 				register(require('./common/enter.directive.js'));
 				register(require('./common/register-state.directive.js'));
 				register(require('./common/uid.service.js'));
+				register(require('./modules/page-builder/aside.controller.js'));
+				register(require('./modules/page-builder/main.controller.js'));
 				register(require('./modules/file-manager/aside.controller.js'));
 				register(require('./modules/file-manager/main.controller.js'));
 				register(require('./modules/file-manager/draggable.directive.js'));
@@ -1787,22 +1789,20 @@ module.exports = function (app) {
 				register(require('./modules/file-manager/file-manager.service.js'));
 				register(require('./modules/model-builder/model-builder.controller.js'));
 				register(require('./modules/model-builder/model-builder.service.js'));
-				register(require('./modules/page-builder/aside.controller.js'));
-				register(require('./modules/page-builder/main.controller.js'));
 				register(require('./modules/resource/resource-create.controller.js'));
 				register(require('./modules/resource/resource-edit.controller.js'));
 				register(require('./modules/resource/resource-index.controller.js'));
 				register(require('./modules/model-builder/components/component.service.js'));
 				register(require('./modules/model-builder/components/checkbox/checkbox-options.directive.js'));
 				register(require('./modules/model-builder/components/checkbox/checkbox.directive.js'));
-				register(require('./modules/model-builder/components/owner/owner-options.directive.js'));
-				register(require('./modules/model-builder/components/owner/owner.directive.js'));
 				register(require('./modules/model-builder/components/dropdown/dropdown-options.directive.js'));
 				register(require('./modules/model-builder/components/dropdown/dropdown.directive.js'));
-				register(require('./modules/model-builder/components/text-multi/text-multi-options.directive.js'));
-				register(require('./modules/model-builder/components/text-multi/text-multi.directive.js'));
+				register(require('./modules/model-builder/components/owner/owner-options.directive.js'));
+				register(require('./modules/model-builder/components/owner/owner.directive.js'));
 				register(require('./modules/model-builder/components/relation/relation-options.directive.js'));
 				register(require('./modules/model-builder/components/relation/relation.directive.js'));
+				register(require('./modules/model-builder/components/text-multi/text-multi-options.directive.js'));
+				register(require('./modules/model-builder/components/text-multi/text-multi.directive.js'));
 				register(require('./modules/model-builder/components/text-single/text-single-options.directive.js'));
 				register(require('./modules/model-builder/components/text-single/text-single.directive.js'));
 
--- resources/assets/js/register.js
+++ resources/assets/js/register.js
@@ -3,6 +3,8 @@ module.exports = app => {
 	register(require('./common/enter.directive.js'));
 	register(require('./common/register-state.directive.js'));
 	register(require('./common/uid.service.js'));
+	register(require('./modules/page-builder/aside.controller.js'));
+	register(require('./modules/page-builder/main.controller.js'));
 	register(require('./modules/file-manager/aside.controller.js'));
 	register(require('./modules/file-manager/main.controller.js'));
 	register(require('./modules/file-manager/draggable.directive.js'));
@@ -10,22 +12,20 @@ module.exports = app => {
 	register(require('./modules/file-manager/file-manager.service.js'));
 	register(require('./modules/model-builder/model-builder.controller.js'));
 	register(require('./modules/model-builder/model-builder.service.js'));
-	register(require('./modules/page-builder/aside.controller.js'));
-	register(require('./modules/page-builder/main.controller.js'));
 	register(require('./modules/resource/resource-create.controller.js'));
 	register(require('./modules/resource/resource-edit.controller.js'));
 	register(require('./modules/resource/resource-index.controller.js'));
 	register(require('./modules/model-builder/components/component.service.js'));
 	register(require('./modules/model-builder/components/checkbox/checkbox-options.directive.js'));
 	register(require('./modules/model-builder/components/checkbox/checkbox.directive.js'));
-	register(require('./modules/model-builder/components/owner/owner-options.directive.js'));
-	register(require('./modules/model-builder/components/owner/owner.directive.js'));
 	register(require('./modules/model-builder/components/dropdown/dropdown-options.directive.js'));
 	register(require('./modules/model-builder/components/dropdown/dropdown.directive.js'));
-	register(require('./modules/model-builder/components/text-multi/text-multi-options.directive.js'));
-	register(require('./modules/model-builder/components/text-multi/text-multi.directive.js'));
+	register(require('./modules/model-builder/components/owner/owner-options.directive.js'));
+	register(require('./modules/model-builder/components/owner/owner.directive.js'));
 	register(require('./modules/model-builder/components/relation/relation-options.directive.js'));
 	register(require('./modules/model-builder/components/relation/relation.directive.js'));
+	register(require('./modules/model-builder/components/text-multi/text-multi-options.directive.js'));
+	register(require('./modules/model-builder/components/text-multi/text-multi.directive.js'));
 	register(require('./modules/model-builder/components/text-single/text-single-options.directive.js'));
 	register(require('./modules/model-builder/components/text-single/text-single.directive.js'));
 
