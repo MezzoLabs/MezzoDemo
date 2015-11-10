e6bdeea708881784e27cdfd50a3e7098dd849f0c
Simon Schneider <trigger3@hotmail.de>
2015-11-10 09:42:09 +0100

    Removed debug output

--- packages/mezzolabs/mezzo/src/Providers/EventServiceProvider.php
+++ packages/mezzolabs/mezzo/src/Providers/EventServiceProvider.php
@@ -64,10 +64,9 @@ class EventServiceProvider extends ServiceProvider
         $dispatcher->listen('*', function ($param = null, $param2 = null) use ($dispatcher) {
         });
 
-        $dispatcher->listen('eloquent.saving*', function ($param = null, $param2 = null) use ($dispatcher) {
-            mezzo_dump($dispatcher->firing());
+        $dispatcher->listen('eloquent.', function ($param = null, $param2 = null) use ($dispatcher) {
+            //mezzo_dump($dispatcher->firing());
         });
-
     }
 
     /**
