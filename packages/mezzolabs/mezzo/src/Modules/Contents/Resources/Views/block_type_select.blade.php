111d08a5100f150979d60e22632b0b285ad2d013
Simon Schneider <trigger3@hotmail.de>
2015-11-21 19:42:42 +0100

    Some Style

--- packages/mezzolabs/mezzo/src/Modules/Contents/Resources/Views/block_type_select.blade.php
+++ packages/mezzolabs/mezzo/src/Modules/Contents/Resources/Views/block_type_select.blade.php
@@ -1,8 +1,19 @@
 <div class="panel-body row">
-    @foreach($blocks as $block)
-        <button type="button" class="btn btn-small btn-default" ng-click="vm.addContentBlock('{{ $block->key() }}', '{{ $block->title() }}', '{{ $block->hash() }}', '{{ $block->propertyInputName('class') }}')">
-            <i class="{{ $block->icon() }}"></i>
-            {{ $block->title() }}
-        </button>
-    @endforeach
+    <div class="col-md-8">
+        <h3>Content Blocks</h3>
+    </div>
+    <div class="col-md-4">
+        <h3>Block Types</h3>
+
+        <div class="list-group">
+            @foreach(\MezzoLabs\Mezzo\Modules\Contents\Types\BlockTypes\ContentBlockTypeRegistrar::make()->all() as $block)
+                <button type="button" class="list-group-item"
+                        ng-click="vm.addContentBlock('{{ $block->key() }}', '{{ $block->title() }}', '{{ $block->hash() }}', '{{ $block->propertyInputName('class') }}')">
+                    <i class="{{ $block->icon() }}"></i>
+                    {{ $block->title() }}
+                </button>
+            @endforeach
+        </div>
+    </div>
+
 </div>
