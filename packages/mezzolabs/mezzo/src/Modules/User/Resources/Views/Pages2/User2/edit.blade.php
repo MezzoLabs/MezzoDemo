b01e1d0a02b1b4ed86667579d7b4859941ef6c5e
Simon Schneider <simon@schneider.click>
2016-02-16 10:03:27 +0100

    Fix git folder case error

--- packages/mezzolabs/mezzo/src/Modules/User/Resources/Views/Pages2/User2/edit.blade.php
+++ packages/mezzolabs/mezzo/src/Modules/User/Resources/Views/Pages2/User2/edit.blade.php
@@ -3,14 +3,15 @@
 @section('main_panel:before')
     <div class="row">
         <div class="col-md-8">
-@endsection
+            @endsection
 
-@section('main_panel:after')
+            @section('main_panel:after')
         </div>
         <div class="col-md-4">
             <div class="panel">
                 <div class="panel-body">
-                    <a href="{!! angular_route('cockpit::user.subscriptions', 'vm.modelId') !!}" class="btn btn-block btn-default"><i class="ion-bookmark"></i> Subscriptions</a>
+                    <a href="{!! angular_route('cockpit::user.subscriptions', 'vm.modelId') !!}"
+                       class="btn btn-block btn-default"><i class="ion-bookmark"></i> Subscriptions</a>
                 </div>
             </div>
         </div>
